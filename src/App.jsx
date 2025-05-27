13cc85db476dec9618ac32ab74c1cce6af59a938
Rayyan J <144414837+RayyanJ2003@users.noreply.github.com>
2025-05-27 18:38:10 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import Header from "./components/Header/Header";
 import Sidebar from "./components/Sidebar/Sidebar";
 import AnatomySection from "./components/AnatomySection/AnatomySection";
-import Calendar from "./components/CalendarPart/calendar";
+import Calendar from "./components/CalendarPart/Calendar";
 import UpcomingSchedule from "./components/UpcomingSchedule/UpcomingSchedule";
 import styles from "./App.module.css";
 
